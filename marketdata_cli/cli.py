517c2db41f106d725610ccf67a702f630e6215b4
r0nny8000 <r0nny8000@bea2b.de>
2021-06-07 22:51:58 +0200

    added defualt for testing

--- marketdata_cli/cli.py
+++ marketdata_cli/cli.py
@@ -5,10 +5,11 @@ import json
 
 from marketdata_cli.yfinancedata import ticker
 
-def getdata(symbol):
+def getdata(symbol='MMM'):
     result = ticker(symbol)
 
     print(json.dumps(result, indent=4))
+    print(result['sector'])
 
 if __name__ == '__main__':
     fire.Fire(getdata)
